0c6442361b8b6eb6a2980a3b2e3dcfa1514806f8
Kumaking420 <152675115+Kumaking420@users.noreply.github.com>
2023-12-03 06:56:57 -0800

    Update config.js

--- config.js
+++ config.js
@@ -7,12 +7,12 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['923353505085', 'Ahmed', true],
+  ['923406234274', 'Ash', true], 
   [''] 
-] //Number of owners
+] //Number of owners 
 
-//global.pairingNumber = "" //put your bot number here
+//global.pairingNumber = "923353505085" //put your bot number her
 
 global.mods = ['917605902011','32460248586'] 
 global.prems = ['917605902011', '32460248586', '919398758484']
